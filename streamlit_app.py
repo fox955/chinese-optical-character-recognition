d8718228009fd4026314f99d747c7f1c10a632e9
fox955 <2230768313@qq.com>
2025-04-19 12:42:48 +0000

    Chinese Character Recognition

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,32 @@
+import io
 import streamlit as st
+import os
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+result = os.popen('pip list').read()
+st.code(result, language=None)
+
+from PIL import Image
+from cnocr import CnOcr
+
+
+def load_image():
+    uploaded_file = st.file_uploader(label='Выберите изображение для распознавания')
+    if uploaded_file is not None:
+        image_data = uploaded_file.getvalue()
+        st.image(image_data)
+        return Image.open(io.BytesIO(image_data))
+    else:
+        return None
+
+
+st.title('🎈 Распознование китайского языка с изображения')
+img = load_image()
+
+result = st.button('Распознать изображение')
+
+if result:
+    text = CnOcr().ocr(img)
+    st.write('**Результаты распознавания:**')
+    st.write("\n".join(i["text"] for i in text))
+    # Объединяет распознанные текстовые фрагменты в одну строку с разделением по строкам (через \n)
+    
